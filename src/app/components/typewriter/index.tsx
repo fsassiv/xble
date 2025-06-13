4d44ab3d28913771f3d3b1ea1325595237dd7971
Flavio Andrade <flavio.ssiv@gmail.com>
2025-06-13 14:03:52 -0300

    done

---
+++ src/app/components/typewriter/index.tsx
@@ -0,0 +1,25 @@
+'use client';
+import Typewriter from 'typewriter-effect';
+import { techWordsNoBle } from '../../words';
+
+export const TypewriterComponent = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-7xl font-bold mb-4 bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+        [X]BLE
+      </h1>
+      <h1 className="font-[100] text-4xl flex text-white">
+        [
+        <Typewriter
+          options={{
+            strings: techWordsNoBle,
+            autoStart: true,
+            loop: true,
+            pauseFor: 1000,
+          }}
+        />
+        ] ble
+      </h1>
+    </div>
+  );
+};
